e8c37d428dbc41f2ce1fb03b9a05f2ddc53f0087
usmanwahyuhp <usmanwahyuhp@gmail.com>
2020-12-12 17:18:05 +0700

    First handling routes

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+import usersRoutes from './routes/users.js';
+
 const app = express();
 const PORT = 5000;
 
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-    console.log('[TEST]');
+app.use('/users', usersRoutes);
 
-    res.send("Welcome to the Users API!");
-});
+app.get("/", (req, res) => res.send("Welcome to the Users API!"));
 
 app.listen(PORT, () =>console.log(`Server Running on port: http://localhost:${PORT}`));
