54db1a7e527e185c85495293f41593822e61037a
usmanwahyuhp <usmanwahyuhp@gmail.com>
2020-12-12 08:09:22 +0700

    Create First Route

--- index.js
+++ index.js
@@ -6,4 +6,10 @@ const PORT = 5000;
 
 app.use(bodyParser.json());
 
+app.get("/", (req, res) => {
+    console.log('[TEST]');
+
+    res.send("Welcome to the Users API!");
+});
+
 app.listen(PORT, () =>console.log(`Server Running on port: http://localhost:${PORT}`));
