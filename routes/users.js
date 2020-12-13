434192ce7fd4996a0a1c67c449f9778d1246e43d
usmanwahyuhp <usmanwahyuhp@gmail.com>
2020-12-13 13:19:13 +0700

    Handling router post

--- routes/users.js
+++ routes/users.js
@@ -7,11 +7,25 @@ const users = [
         firstName : "Muhammad",
         lastName : "Usman",
         age : 25
+    },
+    {
+        firstName : "Wahyu",
+        lastName : "Hidayat",
+        age : 24
     }
 ]
 
 router.get('/', ( req, res ) => {
     res.send(users);
+});
+
+// Add functionality to adding use to database using router post
+router.post('/', ( req, res ) => {
+    const user = req.body;
+
+    users.push(user);
+    
+    res.send("POST routes reached");
 });
 
 export default router;
