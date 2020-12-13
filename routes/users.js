352ffbc08a3de73c3acd8ee61ddab6024b16051b
usmanwahyuhp <usmanwahyuhp@gmail.com>
2020-12-14 00:33:29 +0700

    Make Controllers

--- routes/users.js
+++ routes/users.js
@@ -1,51 +1,13 @@
 import express from 'express';
-import { v4 as uuidv4, v4 } from 'uuid';
+
+import { getUsers, createUser, getUser, deleteUser, updateUser } from '../controllers/users.js';
 
 const router = express.Router();
 
-let users = [];
-
-router.get('/', ( req, res ) => {
-    res.send(users);
-});
-
-// Add functionality to adding use to database using router post
-router.post('/', ( req, res ) => {
-    const user = req.body;
-    
-    // // using spread operator to add id into user data
-    users.push({ ...user, id: uuidv4() });
-    
-    res.send(`User with name ${user.firstName} added to the database`);
-});
-
-router.get('/:id', ( req, res ) => {
-    const { id } = req.params;
-
-    const foundUser = users.find((user) => user.id === id);
-
-    res.send(foundUser);
-});
-
-router.delete('/:id', ( req, res ) => {
-    const { id } = req.params;
-
-    users = users.filter((user) => user.id !== id);
-
-    res.send(`User with id ${id} deleted from database`);
-});
-
-router.patch('/:id', ( req, res ) => {
-    const { id } = req.params;
-    const { firstName, lastName, age } = req.body;
-    
-    const userToBeUpdated = users.find((user) => user.id === id);
-
-    if(firstName) user.firstName = firstName;
-    if(lastName) user.lastName = lastName;
-    if(age) user.age = age;
-
-    res.send(`User with id ${id} has been updated`);
-});
+router.get('/', getUsers);
+router.post('/', createUser);
+router.get('/:id', getUser);
+router.delete('/:id', deleteUser);
+router.patch('/:id', updateUser);
 
 export default router;
